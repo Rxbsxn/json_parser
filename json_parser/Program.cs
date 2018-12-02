b17dbe3229ebf4a04201421f549ca3334a65f37f
Harezlak Robert <harezlak.robert7@gmail.com>
2018-12-02 18:52:46 +0100

    Extract User class to separeted file

--- json_parser/Program.cs
+++ json_parser/Program.cs
@@ -9,21 +9,6 @@ using System.Threading.Tasks;
 
 namespace json_parser
 {
-  class User
-  {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Username { get; set; }
-    public string Email { get; set; }
-    public string Phone { get; set; }
-    public string Website { get; set; }
-
-    public override string ToString()
-    {
-      return $"ID: {Id}, Name: {Name}";
-    }
-
-  }
   class Program
   {
     private static readonly HttpClient client = new HttpClient();
