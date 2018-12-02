158dbb361959e3c34a89e9cad7a72d5eaaacce96
Harezlak Robert <harezlak.robert7@gmail.com>
2018-12-02 17:56:59 +0100

    Add method to get request

--- json_parser/Program.cs
+++ json_parser/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,22 @@ namespace json_parser
 {
   class Program
   {
-    static void Main(string[] args)
+    private static readonly HttpClient client = new HttpClient();
+
+    public static void Main(string[] args)
     {
+      var response = GetResponse("https://jsonplaceholder.typicode.com/users").Result;
+      Console.WriteLine(response);
+      Console.ReadLine();
+    }
+
+    public static async Task<string> GetResponse(string url)
+    {
+      HttpResponseMessage response = await client.GetAsync(url);
+      response.EnsureSuccessStatusCode();
+
+      string responseBody = await response.Content.ReadAsStringAsync();
+      return responseBody;
     }
   }
 }
